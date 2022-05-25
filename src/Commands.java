9e3dff1b022d08ec1422c63ce4301d3c2d121d8c
Avenstor <avenstor1597@gmail.com>
2022-05-26 00:24:40 +0200

    add tip of the day

--- src/Commands.java
+++ src/Commands.java
@@ -2,11 +2,15 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Commands extends ListenerAdapter {
-    public void onMessageReceived(MessageReceivedEvent event){
-        if (event.getMessage().getContentRaw().equalsIgnoreCase("!goha")){
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!goha")) {
             event.getChannel().sendMessage("3 zł").queue();
         }
-        if (event.getMessage().getContentRaw().equalsIgnoreCase("!weather")){
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!tip")) {
+            Tips tips = new Tips();
+            event.getChannel().sendMessage(tips.rollRandomTip()).queue();
+        }
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!weather")) {
             Weather weather = new Weather();
             event.getChannel().sendMessage(weather.checkWeather()).queue();
         }
